3bdd1bd6c2b4f3bbaf32e916be87a4982c759176
Ana Carolina <carol.andradechagas@gmail.com>
2023-05-04 13:43:28 -0300

    Winning conditions

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,4 +1,4 @@
-const Cell = ({ id, cell, setCells, go, setGo }) => {
+const Cell = ({ id, cell, setCells, go, setGo, cells, winningMessage }) => {
   const handleClick = (e) => {
     //Check if the element has either a element of cross or circle
     const taken =
@@ -8,17 +8,31 @@ const Cell = ({ id, cell, setCells, go, setGo }) => {
     if (!taken) {
       if (go === "circle") {
         e.target.firstChild.classList.add("circle");
+        handleCellChange("circle");
         setGo("cross");
       }
       if (go === "cross") {
         e.target.firstChild.classList.add("cross");
+        handleCellChange("cross");
         setGo("circle");
       }
     }
   };
 
+  const handleCellChange = (className) => {
+    const nextCells = cells.map((cell, index) => {
+      //if index equals the id that we are passing
+      if (index === id) {
+        return className;
+      } else {
+        return cell;
+      }
+    });
+    setCells(nextCells);
+  };
+
   return (
-    <div className="square" id={id} onClick={handleClick}>
+    <div className="square" id={id} onClick={!winningMessage && handleClick}>
       <div className="cell"></div>
     </div>
   );
